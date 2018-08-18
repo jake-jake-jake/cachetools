cf103ef074138693b6ecfebcd11694cd7cc23545
Jake Davis <jcdavis@awedge.net>
2018-08-18 14:06:43 -0400

    Scaffolding for dictlike cache

---
+++ cachetools/caches.py
@@ -0,0 +1,46 @@
+from collections.abc import MutableMapping
+
+class Cache(MutableMapping):
+    """ Cache class that presents itself like a dict. On key assignment, if value is a callable,
+        it will only be called on key look up."""
+    
+    def __init__(self, max_size, *args, **kwargs):
+        self.max_size = max_size
+        self._cache = {}
+
+    def __getitem__(self, key):
+        if key in self._cache:
+            func_or_val, fetched = self._cache[key]
+            if fetched is None:
+                fetched = func_or_val.__call__() if hasattr(func_or_val, '__call__') else func_or_val
+                self._cache[key] = func_or_val, fetched
+            return fetched
+        else:
+            raise KeyError
+
+    def __setitem__(self, key, value):
+        self._cache[key] = value, None
+
+    def __delitem__(self, key):
+        del self._cache[key]
+
+    def __len__():
+        return len(self._cache)
+
+    def __iter__(self):
+        return iter(self._cache)
+
+    def reset(self, key=None):
+        """ Remove stored values from cache. """
+        if key is not None:
+            if key not in self:
+                raise KeyError('Key not in Cache: %s' % key)
+            tup = self._cache[key]
+            self._cache[key] = tup[0], None
+            return
+        
+        for key, tup in self._cache.items():
+            self._cache[key] = tup[0], None
+
+
+           
---
+++ tests/test_cache.py
@@ -0,0 +1,34 @@
+from cachetools.caches import Cache
+
+NUMBER_OF_CALLS = 0
+
+def do_a_call():
+    global NUMBER_OF_CALLS
+    NUMBER_OF_CALLS += 1
+    return 'value'
+
+def test_cache():
+    global NUMBER_OF_CALLS
+    assert NUMBER_OF_CALLS == 0
+    
+    # init cache and give it both callables and values
+    c = Cache(10)
+    c['key'] = do_a_call
+    c['another key'] = 5
+    assert NUMBER_OF_CALLS == 0
+    
+    # ensure that callable returns right value
+    assert c['key'] == 'value'
+    assert c['another key'] == 5
+    assert NUMBER_OF_CALLS == 1
+
+    # ensure that fetching the value again does not cause another function call
+    assert c['key'] == 'value'
+    assert NUMBER_OF_CALLS == 1
+
+    # reset cache and then assert that we call function again
+    c.reset()
+    assert NUMBER_OF_CALLS == 1
+    assert c['key'] == 'value'
+    assert NUMBER_OF_CALLS == 2
+
